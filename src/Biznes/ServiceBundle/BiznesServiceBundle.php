17dbe184f4b36321916aca39614c034ca8a729f8
abecek <abcq12013@gmail.com>
2018-06-22 23:33:32 +0200

    reload all files

--- src/Biznes/ServiceBundle/BiznesServiceBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Biznes\ServiceBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BiznesServiceBundle extends Bundle
-{
-}
--- src/Biznes/ShopBundle/BiznesShopBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Biznes\ShopBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BiznesShopBundle extends Bundle
-{
-}
